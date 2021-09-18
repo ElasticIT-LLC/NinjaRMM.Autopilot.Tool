2e2d4e23dd2435484c133b41f9cf3a007b393d06
Anthony Birone <anthonyb@elasticit.com>
2021-09-18 15:48:33 -0400

    Work on string issues with API access

--- Source/Private/Get-NinjaRMMDevice.ps1
+++ Source/Private/Get-NinjaRMMDevice.ps1
@@ -1,4 +1,4 @@
-function Get-NinjaRMMDevices {
+function Get-NinjaRMMDevice {
     
     [CmdletBinding()]
     param(
@@ -12,7 +12,7 @@ function Get-NinjaRMMDevices {
     )
     $NinjaRMMAccessToken = ConvertTo-SecureString $NinjaRMMAccessToken -AsPlainText -Force
     try {
-        $NinjaRMMDevicesWithAutopilotHwid = Invoke-RestMethod -Method Get -Uri "https://app.ninjarmm.com/v2/queries/custom-fields?df=org=1&fields=autopilotHwid" -Authentication OAuth -Token $NinjaRMMAccessToken  | Select-Object -ExpandProperty results
+        $NinjaRMMDevicesWithAutopilotHwid = Invoke-RestMethod -Method Get -Uri "https://app.ninjarmm.com/v2/queries/custom-fields?df=org=1&fields=autopilotHwid" -Authentication OAuth -Token $NinjaRMMAccessToken | Select-Object -ExpandProperty results
     }
     catch {
         Write-Error -Message "Error fetching devices from the NinjaRMM API."
@@ -20,3 +20,5 @@ function Get-NinjaRMMDevices {
 
     return $NinjaRMMDevicesWithAutopilotHwid
 }
+
+Get-NinjaRMMDevice -NinjaRMMAccessToken "OoZOycMaCkcooNSUXO9m3tEl1tLzcpH3ILdymXW9aDk.vBv1vQTTd2zury-aO6cEUCGNDEYjlqwxghH1R_Jmy3g" -NinjaRMMOrganizationID "1"
